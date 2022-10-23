dd7e756977419416824da467f0a1491ca03450e6
Pavel Arefyev <dedundead228@gmail.com>
2022-10-23 14:28:22 +0300

    Use new I2C API for accelerometer

--- main/sensors/accelerometer.cpp
+++ main/sensors/accelerometer.cpp
@@ -39,69 +39,65 @@ int Accelerometer::read(acc_measurements& buffer)
  * Reading will be decreased to 8 bits, therefore resolution decreases
  * @return 0 on success, error status on error
  */
-int Accelerometer::enable_in_fast_mode()
+int Accelerometer::enable_in_fast_mode(const uint& timeout = 0)
 {   
     disable();
 
     // Write new configuration
     uint8_t config = 0b00001111;
-    if (i2c->write(address, CTRL_REG1, &config, 1) == PICO_ERROR_GENERIC) {
-        return ACC_ERROR_STATUS;
-    }
-    fast_mode = true;
+    esp_err_t result = i2c->write_register(address, CTRL_REG1, &config, 1, timeout);
+    if (result == ESP_OK) fast_mode = true;
 
-    sleep_ms(10);
+    vTaskDelay(10);
 
     return 0;
 }
 
 /**
  * @brief Enable accelerometer in normal mode
- * @return 0 on success, error status on error
+ * @return ESP operation status code
  */
-int Accelerometer::enable()
+int Accelerometer::enable(const uint& timeout = 0)
 {
-    disable();
+    disable(timeout);
 
     uint8_t config = 0b00001101;
-    if (i2c->write(address, CTRL_REG1, &config, 1) == PICO_ERROR_GENERIC) {
-        return ACC_ERROR_STATUS;
-    }
-    fast_mode = false;
+    esp_err_t result = i2c->write_register(address, CTRL_REG1, &config, 1, timeout);
+    if (result == ESP_OK) fast_mode = false;
     
-    sleep_ms(10);
+    vTaskDelay(10);
 
-    return 0;
+    return result;
 }
 
 /**
  * @brief Disable accelerometer
- * @return 0 on success, error status on error
+ * @param timeout Timeout for the transaction 
+ * @return ESP operation status code
  */
-int Accelerometer::disable()
-{
-    // Set control register to read values in Xmsb, Ymsb, Zmsb order
-    uint8_t config = 0b00001000;
-    if (i2c->write(address, CTRL_REG1, &config, 1) == PICO_ERROR_GENERIC) {
-        return ACCEL_ERROR_STATUS;
-    }
+esp_err_t Accelerometer::disable(const uint& timeout = 0)
+{   
+    esp_err_t result = i2c->write_register(address, ACCEL_CTRL_REG1, &config, 1, timeout);
+    vTaskDelay(10);
 
-    sleep_ms(10);
-    
-    return 0;
+    return result;
 }
 
 /**
  * @brief Fetch raw data from the device
- * @return Number of bytes read
+ * @return ESP operation status code
  */
-int Accelerometer::fetch_data()
+esp_err_t Accelerometer::fetch_data(const uint& timeout = 0)
 {
     if (fast_mode) {
-        return i2c->read(address, ACCEL_OUT_X_MSB, buffer_short, ACCEL_XYZ_BUFFER);
+        return i2c->read_register(address, ACCEL_OUT_X_MSB,
+                                  buffer_short, ACCEL_XYZ_BUFFER, 
+                                  timeout);
     }
     else {
-        return i2c->read(address, ACCEL_OUT_X_MSB, buffer_full, ACCEL_XYZ_BUFFER_FULL);
+        return i2c->read_register(address, ACCEL_OUT_X_MSB,
+                                  buffer_full, ACCEL_XYZ_BUFFER_FULL,
+                                  timeout);
     }
 }
 
