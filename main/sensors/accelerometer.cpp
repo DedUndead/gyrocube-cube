964368e0479097690f760c0c784e13232994393a
Pavel Arefyev <dedundead228@gmail.com>
2022-10-24 19:57:57 +0300

    Refactor accelerometer interface

--- main/sensors/accelerometer.cpp
+++ main/sensors/accelerometer.cpp
@@ -2,69 +2,58 @@
 #include "sensor/Accelerometer.h"
 #include "interface/i2c.h"
 
-Accelerometer::Accelerometer(I2C* i2c_, uint8_t address_) :
-    fast_mode(false),
+Accelerometer::Accelerometer(I2C* i2c_, uint8_t address_, bool fastmode_) :
+    i2c(i2c_),
     address(address_),
-    i2c(i2c_) 
+    fastmode(fastmode_)
 { /* Empty constructor budy */ }
 
 /**
  * @brief Read sensor data
- * @param acc_measurement Buffer to read the data to
- * @return 0 on success, otherwise error status
+ * @param buffer Buffer to read the data to
+ * @param timeout Read operation timeout
+ * @return ESP operation status code 
  */
-int Accelerometer::read(acc_measurements& buffer)
-{
-    if (fetch_data() < ACCEL_XYZ_BUFFER) {
-        return ACCEL_ERROR_STATUS;
-    }
-    
-    // Save data to buffer
+esp_err_t Accelerometer::read(acc_measurements& buffer, const uint& timeout = 0)
+{   
+    // Fetch raw data
+    uint8_t* raw_buffer = buffer_full;
+    uint8_t raw_buffer_len = ACCEL_XYZ_BUFFER_FULL_LEN;
     if (fast_mode) {
-        buffer.x = ((int16_t)(buffer_short[0] << 8) >> 2) * ACCEL_SCALE_FACTOR;
-        buffer.y = ((int16_t)(buffer_short[1] << 8) >> 2)* ACCEL_SCALE_FACTOR;
-        buffer.z = ((int16_t)(buffer_short[2] << 8) >> 2) * ACCEL_SCALE_FACTOR;
+        raw_buffer = buffer_short;
+        raw_buffer_len = ACCEL_XYZ_BUFFER_LEN;
+    }
+    esp_err_t fetch_result = fetch_data(raw_buffer, raw_buffer_len, timeout);
+    if (fetch_result != ESP_OK) return fetch_result;
+    
+    // Apply transfer function to raw data
+    if (fast_mode) {
+        buffer.x = ((int16_t)(raw_buffer[0] << 8) >> 2) * ACCEL_SCALE_FACTOR;
+        buffer.y = ((int16_t)(raw_buffer[1] << 8) >> 2)* ACCEL_SCALE_FACTOR;
+        buffer.z = ((int16_t)(raw_buffer[2] << 8) >> 2) * ACCEL_SCALE_FACTOR;
     }
     else {
-        buffer.x = ((int16_t)((buffer_full[0] << 8) | buffer_full[1]) >> 2) * ACCEL_SCALE_FACTOR;
-        buffer.y = ((int16_t)((buffer_full[2] << 8) | buffer_full[3]) >> 2) * ACCEL_SCALE_FACTOR;
-        buffer.z = ((int16_t)((buffer_full[4] << 8) | buffer_full[5]) >> 2) * ACCEL_SCALE_FACTOR;
+        buffer.x = ((int16_t)((raw_buffer[0] << 8) | raw_buffer[1]) >> 2) * ACCEL_SCALE_FACTOR;
+        buffer.y = ((int16_t)((raw_buffer[2] << 8) | raw_buffer[3]) >> 2) * ACCEL_SCALE_FACTOR;
+        buffer.z = ((int16_t)((raw_buffer[4] << 8) | raw_buffer[5]) >> 2) * ACCEL_SCALE_FACTOR;
     }
 
-    return 0;
-}
-
-/**
- * @brief Enable accelerometer in fast mode
- * Reading will be decreased to 8 bits, therefore resolution decreases
- * @return 0 on success, error status on error
- */
-int Accelerometer::enable_in_fast_mode(const uint& timeout = 0)
-{   
-    disable();
-
-    // Write new configuration
-    uint8_t config = 0b00001111;
-    esp_err_t result = i2c->write_register(address, CTRL_REG1, &config, 1, timeout);
-    if (result == ESP_OK) fast_mode = true;
-
-    vTaskDelay(10);
-
-    return 0;
+    return fetch_result;
 }
 
 /**
  * @brief Enable accelerometer in normal mode
- * @return ESP operation status code
+ * @return ESP operation status code 
  */
-int Accelerometer::enable(const uint& timeout = 0)
+esp_err_t Accelerometer::enable(const uint& timeout = 0) const
 {
     disable(timeout);
 
-    uint8_t config = 0b00001101;
+    uint8_t config;
+    if (fastmode) config = 0b00001111;
+    else          config = 0b00001101;
+
     esp_err_t result = i2c->write_register(address, CTRL_REG1, &config, 1, timeout);
-    if (result == ESP_OK) fast_mode = false;
-    
     vTaskDelay(10);
 
     return result;
@@ -75,8 +64,10 @@ int Accelerometer::enable(const uint& timeout = 0)
  * @param timeout Timeout for the transaction 
  * @return ESP operation status code
  */
-esp_err_t Accelerometer::disable(const uint& timeout = 0)
+esp_err_t Accelerometer::disable(const uint& timeout = 0) const
 {   
+    uint8_t config = 0b00001000;
+
     esp_err_t result = i2c->write_register(address, ACCEL_CTRL_REG1, &config, 1, timeout);
     vTaskDelay(10);
 
@@ -85,38 +76,34 @@ esp_err_t Accelerometer::disable(const uint& timeout = 0)
 
 /**
  * @brief Fetch raw data from the device
+ * @param buffer Target buffer
+ * @param buffer_len Length of the target buffer
+ * @param timeout Read operation timeout
  * @return ESP operation status code
  */
-esp_err_t Accelerometer::fetch_data(const uint& timeout = 0)
+esp_err_t Accelerometer::fetch_data(uint8_t* buffer, const uint8_t& buffer_len, uint& timeout = 0) const
 {
-    if (fast_mode) {
-        return i2c->read_register(address, ACCEL_OUT_X_MSB,
-                                  buffer_short, ACCEL_XYZ_BUFFER, 
-                                  timeout);
-    }
-    else {
-        return i2c->read_register(address, ACCEL_OUT_X_MSB,
-                                  buffer_full, ACCEL_XYZ_BUFFER_FULL,
-                                  timeout);
-    }
+    return i2c->read_register(address, ACCEL_OUT_X_MSB, buffer_full, buffer_len, timeout);
 }
 
 /**
  * @brief Returns current orientation of the cube
+ * @param timeout Read operation timeout
  * @return Side index of the cube
  * On error, return ORIENT_UNKNOWN
  */
-uint8_t Accelerometer::get_side()
-{
-    if (Accelerometer::read(internal_measurements) == ACCEL_ERROR_STATUS) {
+uint8_t Accelerometer::get_side(const uint& timeout = 0) const
+{   
+    accel_measurements reading;
+    if (Accelerometer::read(reading) != ESP_OK) {
         return ACCEL_ORIENT_UNKNOWN;
     };
 
-    if      (internal_measurements.z > ACCEL_THRESHOLD)      return 0;
-    else if (internal_measurements.y < -1 * ACCEL_THRESHOLD) return 1;
-    else if (internal_measurements.x < -1 * ACCEL_THRESHOLD) return 2;
-    else if (internal_measurements.x > ACCEL_THRESHOLD)      return 3;
-    else if (internal_measurements.y > ACCEL_THRESHOLD)      return 4;
-    else if (internal_measurements.z < -1 * ACCEL_THRESHOLD) return 5;
-    else                                                     return ACCEL_ORIENT_UNKNOWN;
+    if      (reading.z > threshold)      return 0;
+    else if (reading.y < -1 * threshold) return 1;
+    else if (reading.x < -1 * threshold) return 2;
+    else if (reading.x > threshold)      return 3;
+    else if (reading.y > threshold)      return 4;
+    else if (reading.z < -1 * threshold) return 5;
+    else                                 return ACCEL_ORIENT_UNKNOWN;
 }
--- main/sensors/accelerometer.h
+++ main/sensors/accelerometer.h
@@ -10,17 +10,17 @@
 #define ACCEL_OUT_X_MSB        0x01
 #define ACCEL_SCALE_FACTOR     (float)0.244
 
-#define ACCEL_XYZ_BUFFER       3
-#define ACCEL_XYZ_BUFFER_FULL  6
-#define ACCEL_ERROR_STATUS     0xEFFF
-#define ACCEL_ORIENT_UNKNOWN   0x06
-#define ACCEL_THRESHOLD        850
+#define ACCEL_XYZ_BUFFER_LEN       3
+#define ACCEL_XYZ_BUFFER_FULL_LEN  6
+#define ACCEL_ERROR_STATUS         0xEFFF
+#define ACCEL_ORIENT_UNKNOWN       0x06
+#define ACCEL_THRESHOLD            850
 
-typedef struct acc_measurements {
+typedef struct accel_measurements {
     int16_t x;
     int16_t y;
     int16_t z;
-} acc_measurements;
+} accel_measurements;
 
 /**
  * @brief Abstraction of accelerometer class
@@ -28,20 +28,21 @@ typedef struct acc_measurements {
  */
 class Accelerometer {
 public:
-    Accelerometer(I2C* i2c_, uint8_t address_ = DEF_ADDRESS);
-    int read(acc_measurements& buffer);
-    int enable_in_fast_mode();
-    int enable();
-    int disable();
-    uint8_t get_side();
+    Accelerometer(I2C* i2c_,
+                  uint8_t address_ = DEF_ADDRESS,
+                  const bool& fastmode_ = false,
+                  const uint16_t& threshold = ACCEL_THRESHOLD);
+    esp_err_t read(acc_measurements& buffer, const uint& timeout = 0) const;
+    esp_err_t enable(const uint& timeout = 0) const;
+    esp_err_t disable(const uint& timeout = 0) const;
+    uint8_t get_side(const uint& timeout = 0) const;
 private:
-    int fetch_data();
+    esp_err_t fetch_data(uint8_t* buffer, const uint8_t& buffer_len, uint& timeout = 0);
 
     bool fast_mode;                             // Flag determines fast mode operation
     uint8_t buffer_short[ACCEL_XYZ_BUFFER];     // Short buffer for fast reading
     uint8_t buffer_full[ACCEL_XYZ_BUFFER_FULL]; // Full buffer for full reading
 
-    acc_measurements internal_measurements;
     uint8_t address;
     I2C* i2c;
 };
